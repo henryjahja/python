e790936659d1b0e16b24ec640620690839ee98a0
Henry Jahja <henryjahja@users.noreply.github.com>
2016-09-12 10:30:37 -0700

    Create day11-2DArrays.py

---
+++ hackerrank/30-days-Coding-Challenge/day11-2DArrays.py
@@ -0,0 +1,34 @@
+''' by Shafaet
+Given a 6 x 6 2D Array, A:
+
+1 1 1 0 0 0
+0 1 0 0 0 0
+1 1 1 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+0 0 0 0 0 0
+
+We define an hourglass in A to be a subset of values with indices falling in this pattern in A's graphical representation:
+
+a b c
+  d
+e f g
+
+There are 16 hourglasses in A, and an hourglass sum is the sum of an hourglass' values.
+
+Task 
+Calculate the hourglass sum for every hourglass in A, then print the maximum hourglass sum.
+'''
+
+#!/bin/python3
+import sys
+arr = []
+for arr_i in range(6):
+   arr_t = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+   arr.append(arr_t)
+res = []
+for x in range(0, 4):
+    for y in range(0, 4):
+        s = sum(arr[x][y:y+3]) + arr[x+1][y+1] + sum(arr[x+2][y:y+3])
+        res.append(s)
+print(max(res))
