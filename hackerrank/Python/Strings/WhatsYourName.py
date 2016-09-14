1f430422cfcf6216964664171381113e107fcd5d
Henry Jahja <henryjahja@users.noreply.github.com>
2016-09-14 13:13:41 -0700

    Create WhatsYourName.py

---
+++ hackerrank/Python/Strings/WhatsYourName.py
@@ -0,0 +1,7 @@
+''' by harsh_beria93
+You are given the firstname and lastname of a person on two different lines. Your task is to read them and print the following:
+
+Hello firstname lastname! You just delved into python.
+'''
+
+print("Hello " + input() + " " + input() + "! You just delved into python.")
