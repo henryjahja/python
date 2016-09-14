fa20c25c04c330b0ad0a03c58e44a41e30619e6f
Henry Jahja <henryjahja@users.noreply.github.com>
2016-09-14 10:00:44 -0700

    Create sWAPcASE.py

---
+++ hackerrank/sWAPcASE.py
@@ -0,0 +1,5 @@
+'''by DOSHI
+You are given a string S. Your task is to swap cases. In other words, convert all lowercase letters to uppercase letters and vice versa.
+'''
+
+print(input().swapcase())
