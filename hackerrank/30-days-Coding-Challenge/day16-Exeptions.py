78831759140b3739b0db7ce3902015ed6397ada2
Henry Jahja <henryjahja@users.noreply.github.com>
2016-09-18 19:27:51 -0700

    Create day16-Exeptions.py

---
+++ hackerrank/30-days-Coding-Challenge/day16-Exeptions.py
@@ -0,0 +1,8 @@
+''' by AvimanyuSingh
+Read a string, S, and print its integer value; if S cannot be converted to an integer, print Bad String.
+'''
+
+try:
+    print(int(input()))
+except:
+    print("Bad String")
